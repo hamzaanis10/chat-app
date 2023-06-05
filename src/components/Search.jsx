5f90af735e1c16f3e1f848c36e7262e59569dbc3
hamza10 <hamzaanis2014@gmail.com>
2023-06-05 14:25:55 +0500

    Search and sidebar update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
 const Search = () => {
-  return <div>Search</div>;
+  return <div className="search">
+    <div className='searchForm'>
+      <input type="text"></input>
+    </div>
+
+    <div className='userChat'>
+      <img src="https://images.unsplash.com/photo-1685808157817-1611631f8d30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"></img>
+      <div className="userChatInfo">
+        <span>Hamza</span>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Search;
